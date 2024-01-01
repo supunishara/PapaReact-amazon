43b092e83fe8f04a609ddedb0848d30196cb5555
Sa@01126576437 <supun.ishara@axiatadigitallabs.com>
2024-01-02 00:33:22 +0530

    checking github authentication

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,60 @@
+"use client";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
 import ProductFeed from "../components/ProductFeed";
+import { useSession, signIn, signOut } from "next-auth/react";
 
-export default async function Home() {
-  const data = await getData();
+export default function Home() {
+  const { data: session, status } = useSession();
+
+  // const data = await getData();
+
+  if (session) {
+    //logged in code
+    return (
+      <div>
+        <p>Welcome {session.user?.name}. Signed In As</p>
+        <p>{session.user?.email}</p>
+        <button onClick={() => signOut()}>Sign out</button>
+      </div>
+    );
+  }
+
   return (
     <main className="bg-gray-100">
-      <Header />
-      <main className="max-w-screen-2xl mx-auto">
-        {/* banner */}
-        <Banner />
-        {/* ProductFeed */}
-        <ProductFeed products={data} />
-      </main>
+      {/* <Header /> */}
+      {/* <main className="max-w-screen-2xl mx-auto"> */}
+      {/* banner */}
+      {/* <Banner /> */}
+      {/* ProductFeed */}
+      {/* <ProductFeed products={data} /> */}
+      {/* </main> */}
+
+      <p>Not Signed In</p>
+      <button
+        className="bg-blue-600 py-2 px-6 rounded-md mb-2"
+        onClick={signIn}
+      >
+        Sign in with google
+      </button>
+      <button
+        className="bg-none border-gray-300 border py-2 px-6 rounded-md mb-2"
+        onClick={signIn}
+      >
+        Sign in with github
+      </button>
     </main>
   );
 }
 
-async function getData() {
-  const products = await fetch("https://fakestoreapi.com/products");
+// async function getData() {
+//   const products = await fetch("https://fakestoreapi.com/products");
 
-  if (!products.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  return products.json();
-}
+//   if (!products.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
+//   return products.json();
+// }
 
 // export async function getServerSideProps(context) {
 //   const products = await fetch("https://fakestoreapi.com/products").then(
